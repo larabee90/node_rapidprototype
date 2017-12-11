55a39a23d2151ba3b82287dc68e5d43c482f60ea
larabee90 <lauradouglas90@gmail.com>
2017-12-11 15:32:17 -0500

    Connected scripts and styles and added socket.js file.

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,14 @@
 /**
  * Created by lauradouglas on 2017-12-09.
  */
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendFile('index.html', {'root': './'});
+});
+
+http.listen(3000, function(){
+    console.log('listening on port 3000');
+});
