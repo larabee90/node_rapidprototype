3c5f48252c62dd51b19198b09ce93f0f8e2473fb
anamerfu <anamerfu@gmail.com>
2018-01-12 22:29:17 -0500

    Merge branch 'master' into Ana
    # Conflicts:
    #	index.html
    
    Added chat div
    chat msging works

--- index.js
+++ index.js
@@ -4,37 +4,34 @@
 var express = require('express');
 var http = require('http');
 var socketio = require('socket.io');
-var path = require('path');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+
+var server =  http.createServer(app).listen(3000);
+var io = socketio(server);
+
+app.use(express.static('./public'));
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
-});
+ });
 
-var server =  http.createServer(app);
-var io = socketio.listen(server);
-server.listen(3000);
-// app.listen(3000);
+
+
 console.log('app listening on port 3000');
-// var io = socketio.listen(server);
 
-var linesDrawn = [];
 
 // event-handler for new incoming connections
-io.on('connection', function (socket) {
+io.on("connection", function(socket) {
 
-    // first send the history to the new client
-    for (var i in linesDrawn) {
-        socket.emit('draw_line', { line: linesDrawn[i] } );
-    }
+    socket.on("chat", function(message) {
+        io.emit("message", message);
 
-    // add handler for message type "draw_line".
-    socket.on('draw_line', function (data) {
-        // add received line to history
-        linesDrawn.push(data.line);
-        // send line to all clients
-        io.emit('draw_line', { line: data.line });
+        console.log(message);
     });
+
+    socket.emit("message", "Welcome to Cyber Chat");
+
 });
+
+console.log("Starting Socket App - http://localhost:3000");
--- public/js/app.js
+++ public/js/app.js
@@ -1,121 +1,42 @@
 /**
  * Created by lauradouglas on 2017-12-11.
  */
-document.addEventListener("DOMContentLoaded", function() {
-    var mouse = {
-        click: false,
-        move: false,
-        pos: {x:0, y:0},
-        pos_prev: false
-    };
-    // get canvas element and create context
-    var canvas  = document.getElementById('paintCanvas');
-    var context = canvas.getContext('2d');
-    var width   = window.innerWidth;
-    var height  = window.innerHeight;
-    var socket  = io.connect();
 
-    // set canvas to full browser width/height
-    canvas.width = width;
-    canvas.height = height;
 
-    // register mouse event handlers
-    canvas.onmousedown = function(e){ mouse.click = true; };
-    canvas.onmouseup = function(e){ mouse.click = false; };
+//chat tings
 
-    canvas.onmousemove = function(e) {
-        // normalize mouse position to range 0.0 - 1.0
-        mouse.pos.x = e.clientX / width;
-        mouse.pos.y = e.clientY / height;
-        mouse.move = true;
-    };
 
-    // draw line received from server
-    socket.on('draw_line', function (data) {
-        var line = data.line;
-        context.beginPath();
-        context.moveTo(line[0].x * width, line[0].y * height);
-        context.lineTo(line[1].x * width, line[1].y * height);
-        context.stroke();
-    });
+var socket = io("http://localhost:3000");
 
-    // main loop, running every 25ms
-    function mainLoop() {
-        // check if the user is drawing
-        if (mouse.click && mouse.move && mouse.pos_prev) {
-            // send line to to the server
-            socket.emit('draw_line', { line: [ mouse.pos, mouse.pos_prev ] });
-            mouse.move = false;
-        }
-        mouse.pos_prev = {x: mouse.pos.x, y: mouse.pos.y};
-        setTimeout(mainLoop, 25);
-    }
-    mainLoop();
+socket.on("disconnect", function() {
+    setTitle("Disconnected");
 });
-//
-// var canvas = document.getElementById('paintCanvas');
-// var ctx = canvas.getContext('2d');
-// ctx.lineWidth = 5;
-// ctx.strokeStyle = "red";
-// ctx.lineCap = "round";
-// var isDrawing = false;
-//
-// //mouse events
-// canvas.addEventListener('mousemove', drawingLine);
-//
-// canvas.addEventListener('mousedown', function(){
-//     isDrawing = true;
-//     ctx.beginPath();
-//     ctx.moveTo(posX,posY);
-//     canvas.addEventListener('mousemove', drawingLine);
-// });
-//
-// canvas.addEventListener('mouseup', function(){
-//     isDrawing = false;
-// });
-//
-// //draw initial lines on canvas
-// function drawingLine(evt){
-//     posX = evt.pageX - canvas.offsetLeft;
-//     posY = evt.pageY - canvas.offsetTop;
-//
-//     if (isDrawing === true) {
-//         ctx.lineTo(posX, posY);
-//         ctx.stroke();
-//     }
-// };
-//
-// function colourSwitch(color){
-//     ctx.strokeStyle = color;
-// };
-//
-// function changeWidth(lineWidth){
-//     ctx.lineWidth = lineWidth;
-// };
-//
-// //creating the pattern brush
-// function makeBrush(){
-//     var patternCanvas = document.createElement('canvas');
-//     var circleWidth = 15;
-//     var circleDistance = 5;
-//     var patCtx = patternCanvas.getContext('2d');
-//     patternCanvas.width = patternCanvas.height = circleWidth + circleDistance;
-//
-//     patCtx.fillStyle = "#9C27B0";
-//     patCtx.beginPath();
-//     patCtx.arc(circleWidth / 2, circleWidth / 2, circleWidth / 2, Math.PI * 2, false);
-//     patCtx.closePath();
-//     patCtx.fill();
-//     return ctx.createPattern(patternCanvas, 'repeat');
-// };
-//
-// //setting the function for the pattern brush; is called when button is clicked
-// function drawPattern(){
-//     ctx.lineWidth = 30;
-//     ctx.strokeStyle = makeBrush();
-// };
-//
-// //reset the canvas
-// function clearCanvas(){
-//     ctx.clearRect(0,0,canvas.width, canvas.height);
-// };
+
+ socket.on("connect", function() {
+    setTitle("Connected to Cyber Chat");
+ });
+
+socket.on("message", function(message) {
+    printMessage(message);
+});
+
+
+document.forms[0].onsubmit = function () {
+    var input = document.getElementById("message");
+    printMessage(input.value);
+    console.log(input.value);
+    console.log("this runs");
+    socket.emit("chat", input.value);
+    //input.value = '';
+};
+
+function setTitle(title) {
+    document.querySelector("h1").innerHTML = title;
+}
+
+function printMessage(message) {
+    var p = document.createElement("p");
+    p.innerText = message;
+    document.querySelector("div.messages").appendChild(p);
+}
+
